0fca2b70fa53116b548c9c34b74abf322405c0f0
Leo Duffy <leoduffy@hotmail.com>
2017-10-29 10:01:10 +0000

    Setup

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ROOT: 'http://localhost:3000/api',
+  USERNAME: 'northcoder'
+};
+
+// looking after different oarts of the site, baskwet team, checkout team
+// autonomy. teams product owners. cross functional teams
+// external companies coming in , spotify. talkin gabout setup.
+
+// .net company on the back end.
