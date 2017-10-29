7b0e3e20facf845f8a8dfac7830c202b08cec055
Leo Duffy <leoduffy@hotmail.com>
2017-10-29 10:07:22 +0000

    [ArticleCard] Renders the component on fetchArticles

---
+++ src/components/ArticleList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect  } from 'react-redux';
+import PropTypes from 'prop-types';
+import fetchArticles from '../actions/articles';
+import * as actions from '../actions/articles';
+import ArticleCard from './ArticleCard';
+
+export class ArticleList extends React.Component {
+  
+  componentDidMount () {
+    this.props.fetchArticles();
+  }
+
+  render () {
+
+    return (
+      <div>
+        {this.props.articles.sort(function (a,b) { return b.votes - a.votes;})
+          .map(article => 
+            <ArticleCard 
+              articleVote={this.props.articleVote}
+              title={article.title} 
+              votes={article.votes} 
+              id={article._id}
+              key={article.title} 
+              user={article.created_by}/>)}
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    fetchArticles: () => {
+      dispatch(fetchArticles());
+    }, 
+    articleVote:(id, vote) => {
+      dispatch(actions.articleVote(id, vote));
+    }
+  };
+}
+
+function mapStateToProps (state) {
+  return {
+    articles: state.articles.data
+  };
+}
+
+ArticleList.propTypes = {
+  articles: PropTypes.array.isRequired,
+  fetchArticles: PropTypes.func.isRequired,
+  articleVote: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArticleList);
