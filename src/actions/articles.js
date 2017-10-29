38263aac94d48b89dc0169aafee6fb952a6e0786
Leo Duffy <leoduffy@hotmail.com>
2017-10-29 10:13:06 +0000

    Implement get articles

---
+++ src/actions/articles.js
@@ -0,0 +1,40 @@
+
+import axios from 'axios';
+import * as types from './types';
+
+import {
+  ROOT
+} from '../../config';
+
+export default function fetchArticles() {
+  return function (dispatch) {
+    dispatch(fetchArticlesRequest());
+    return axios.get(`${ROOT}/articles`)
+      .then(res => {
+        return dispatch(fetchArticlesSuccess(res.data.articles));
+      })
+      .catch(err => {
+        dispatch(fetchArticlesError(err.message));
+      });
+  };
+}
+
+export function fetchArticlesRequest() {
+  return {
+    type: types.FETCH_ARTICLES_REQUEST
+  };
+}
+
+export function fetchArticlesSuccess(articles) {
+  return {
+    type: types.FETCH_ARTICLES_SUCCESS,
+    articles: articles
+  };
+}
+
+export function fetchArticlesError(error) {
+  return {
+    type: types.FETCH_ARTICLES_ERROR,
+    articles: error
+  };
+}
