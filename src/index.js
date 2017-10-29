d552b2b29b56aa029499fdf5a5998ca479397546
Leo Duffy <leoduffy@hotmail.com>
2017-10-29 10:02:07 +0000

    Setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
+
+import reducer from './reducers';
+import App from './components/App';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}>
+  <App/>
+</Provider>, document.getElementById('app'));
