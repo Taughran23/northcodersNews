10694120b5934758b7e1d1d46ce6209ed3b4339e
Leo Duffy <leoduffy@hotmail.com>
2017-10-30 15:59:26 +0000

    Implement state update

---
+++ src/reducers/topicsArticles.js
@@ -0,0 +1,27 @@
+import * as types from '../actions/types';
+import { initialState } from './index.js';
+
+export default (prevState = initialState, action) => {
+  switch (action.type) {
+  case types.FETCH_TOPIC_ARTICLES_REQUEST:
+    return Object.assign({}, prevState, {
+      loading: !prevState.loading,
+      error: null,
+      data: []
+    });
+  case types.FETCH_TOPIC_ARTICLES_SUCCESS:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: null,
+      data: action.topicArticles
+    });
+  case types.FETCH_TOPIC_ARTICLES_ERROR:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: action.data,
+      data: []
+    });
+  default:
+    return prevState;
+  }
+};
