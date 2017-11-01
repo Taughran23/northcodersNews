3557c54026b4268ff4c0fea191ccd91652e16ff1
Leo Duffy <leoduffy@hotmail.com>
2017-11-01 14:21:16 +0000

    Implement update article vote count

---
+++ src/actions/articleVote.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import * as types from './types';
+
+import {
+  ROOT
+} from '../../config';
+
+export default function articleVote (id, vote) {
+  return function (dispatch) {
+    dispatch(articleVoteRequest());
+    return axios.put(`${ROOT}/articles/${id}?vote=${vote}`)
+      .then(() => {
+        return  dispatch(articleVoteSuccess(id, vote));
+      })
+      .catch((err) => {
+        dispatch(articleVoteError(err));
+      });
+  };
+}
+
+export function articleVoteRequest () {
+  return {
+    type: types.ARTICLE_VOTE_REQUEST
+  };
+}
+
+export function articleVoteSuccess (articleId, vote) {
+  return {
+    type: types.ARTICLE_VOTE_SUCCESS,
+    articleId, 
+    vote
+  };
+}
+
+export function articleVoteError (error) {
+  return {
+    type: types.ARTICLE_VOTE_ERROR,
+    data: error
+  };
+}
