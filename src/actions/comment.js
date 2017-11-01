4982885dc046f4e9d7715a71e57113ca41fce852
Leo Duffy <leoduffy@hotmail.com>
2017-11-01 10:36:46 +0000

    Implement fetchComment

---
+++ src/actions/comment.js
@@ -0,0 +1,40 @@
+
+import axios from 'axios';
+import * as types from './types';
+
+import {
+  ROOT
+} from '../../config';
+
+export default function fetchComments(articleID) {
+  return function (dispatch) {
+    dispatch(fetchCommentsRequest());
+    return axios.get(`${ROOT}/articles/${articleID}/comments`)
+      .then(res => {
+        return dispatch(fetchCommentsSuccess(res.data));
+      })
+      .catch(err => {
+        dispatch(fetchCommentsError(err.message));
+      });
+  };
+}
+
+export function fetchCommentsRequest() {
+  return {
+    type: types.FETCH_COMMENTS_REQUEST
+  };
+}
+
+export function fetchCommentsSuccess(comments) {
+  return {
+    type: types.FETCH_COMMENTS_SUCCESS,
+    data: comments
+  };
+}
+
+export function fetchCommentsError(error) {
+  return {
+    type: types.FETCH_COMMENTS_ERROR,
+    comments: error
+  };
+}
