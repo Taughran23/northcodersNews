fab401bf088a00827f55de525cc10ab1541c503e
Leo Duffy <leoduffy@hotmail.com>
2017-11-01 10:45:39 +0000

    Add tests for fetchComments, fetchArticle, addComments and commentVote

--- src/__test__/reducers.test.js
+++ src/__test__/reducers.test.js
@@ -1,5 +1,5 @@
-import { reducer } from '../reducers/index';
-import initialState from '../reducers/index';
+import reducer  from '../reducer/reducer';
+import {initialState} from '../reducer/reducer';
 import * as types from '../actions/types'; 
 import expect from 'expect';
 
@@ -13,15 +13,15 @@ describe('reducers', ()=>{
         type: types.FETCH_ARTICLES_REQUEST,
       };
       const test = reducer(initialState, action);   
-      expect(test.articles.loading).toEqual(true);
+      expect(test.loading).toEqual(true);
     });
     it('returns with the requested data', ()=> {
       const action = {
         type: types.FETCH_ARTICLES_SUCCESS,
-        articles: ['requested', 'data'],
+        data: ['requested', 'data'],
       };
       const test = reducer(initialState, action); 
-      expect(test.articles.data).toEqual(['requested', 'data']);
+      expect(test.articles).toEqual(['requested', 'data']);
     });
     it('returns an error if passed the error action', () => {
       const action = {
@@ -29,7 +29,7 @@ describe('reducers', ()=>{
         data: 'This is an error message'
       };
       const test = reducer(initialState, action);
-      expect(test.articles.error).toEqual('This is an error message');
+      expect(test.error).toEqual('This is an error message');
     });
   });
   describe('fetchTopicTitles', ()=> {
@@ -38,15 +38,15 @@ describe('reducers', ()=>{
         type: types.FETCH_TOPIC_TITLES_REQUEST,
       };
       const test = reducer(initialState, action);   
-      expect(test.topics.loading).toEqual(true);
+      expect(test.loading).toEqual(true);
     });
     it('returns with the requested data', ()=> {
       const action = {
         type: types.FETCH_TOPIC_TITLES_SUCCESS,
-        topics: ['football', 'coding','cooking'],
+        data: ['football', 'coding','cooking'],
       };
       const test = reducer(initialState, action); 
-      expect(test.topics.data).toEqual(['football', 'coding','cooking']);
+      expect(test.topics).toEqual(['football', 'coding','cooking']);
     });
     it('returns an error if passed the error action', () => {
       const action = {
@@ -54,24 +54,24 @@ describe('reducers', ()=>{
         data: 'This is an error message'
       };
       const test = reducer(initialState, action);
-      expect(test.topics.error).toEqual('This is an error message');
+      expect(test.error).toEqual('This is an error message');
     });
   });
-  describe('fetchTopicArticles', ()=> {
+  describe('fetchArticle', ()=> {
     it('should return newState.loading = true', () => {
       const action = {
         type: types.FETCH_TOPIC_ARTICLES_REQUEST,
       };
       const test = reducer(initialState, action);   
-      expect(test.topicArticles.loading).toEqual(true);
+      expect(test.loading).toEqual(true);
     });
     it('returns with the requested data', ()=> {
       const action = {
         type: types.FETCH_TOPIC_ARTICLES_SUCCESS,
-        topicArticles: [1, 2, 3],
+        data: [1, 2, 3],
       };
       const test = reducer(initialState, action); 
-      expect(test.topicArticles.data).toEqual([1, 2, 3]);
+      expect(test.articles).toEqual([1, 2, 3]);
     });
     it('returns an error if passed the error action', () => {
       const action = {
@@ -79,7 +79,137 @@ describe('reducers', ()=>{
         data: 'This is an error message'
       };
       const test = reducer(initialState, action);
-      expect(test.topicArticles.error).toEqual('This is an error message');
+      expect(test.error).toEqual('This is an error message');
+    });
+  });
+  describe('fetchTopicArticles', ()=> {
+    it('should return newState.loading = true', () => {
+      const action = {
+        type: types.FETCH_ARTICLE_REQUEST,
+      };
+      const test = reducer(initialState, action);   
+      expect(test.loading).toEqual(true);
+    });
+    it('returns with the requested data', ()=> {
+      const action = {
+        type: types.FETCH_ARTICLE_SUCCESS,
+        data: {author: 'northcoder'},
+      };
+      const test = reducer(initialState, action); 
+      expect(test.article).toEqual({author: 'northcoder'});
+    });
+    it('returns an error if passed the error action', () => {
+      const action = {
+        type: types.FETCH_ARTICLE_ERROR,
+        data: 'This is an error message'
+      };
+      const test = reducer(initialState, action);
+      expect(test.error).toEqual('This is an error message');
+    });
+  });
+  describe('fetchArticle', ()=> {
+    it('should return newState.loading = true', () => {
+      const action = {
+        type: types.FETCH_TOPIC_ARTICLES_REQUEST,
+      };
+      const test = reducer(initialState, action);   
+      expect(test.loading).toEqual(true);
+    });
+    it('returns with the requested data', ()=> {
+      const action = {
+        type: types.FETCH_TOPIC_ARTICLES_SUCCESS,
+        data: [1, 2, 3],
+      };
+      const test = reducer(initialState, action); 
+      expect(test.articles).toEqual([1, 2, 3]);
+    });
+    it('returns an error if passed the error action', () => {
+      const action = {
+        type: types.FETCH_TOPIC_ARTICLES_ERROR,
+        data: 'This is an error message'
+      };
+      const test = reducer(initialState, action);
+      expect(test.error).toEqual('This is an error message');
+    });
+  });
+  describe('fetchComment', ()=> {
+    it('should return newState.loading = true', () => {
+      const action = {
+        type: types.FETCH_COMMENTS_REQUEST,
+      };
+      const test = reducer(initialState, action);   
+      expect(test.loading).toEqual(true);
+    });
+    it('returns with the requested data', ()=> {
+      const action = {
+        type: types.FETCH_COMMENTS_SUCCESS,
+        data: [1,2,3],
+      };
+      const test = reducer(initialState, action); 
+      expect(test.comments).toEqual([1,2,3]);
+    });
+    it('returns an error if passed the error action', () => {
+      const action = {
+        type: types.FETCH_COMMENTS_ERROR,
+        data: 'This is an error message'
+      };
+      const test = reducer(initialState, action);
+      expect(test.error).toEqual('This is an error message');
+    });
+  });
+  describe('commentVote', ()=> {
+    it('should return newState.loading = true', () => {
+      const action = {
+        type: types.COMMENT_VOTE_REQUEST,
+      };
+      const test = reducer(initialState, action);   
+      expect(test.loading).toEqual(true);
+    });
+    it('should return newState.loading = true', () => {
+      const commentId = 1;
+      const vote = 'up';
+      const comments = [1,2,3];
+      const action = {
+        type: types.COMMENT_VOTE_SUCCESS,
+        commentId, 
+        vote, 
+        comments
+      };
+      const test = reducer(initialState, action); 
+      expect(test.loading).toEqual(false);
+    });
+    it('returns an error if passed the error action', () => {
+      const action = {
+        type: types.COMMENT_VOTE_ERROR,
+        data: 'This is an error message'
+      };
+      const test = reducer(initialState, action);
+      expect(test.error).toEqual('This is an error message');
+    });
+  });
+  describe('addComment', ()=> {
+    it('should return newState.loading = true', () => {
+      const action = {
+        type: types.ADD_COMMENT_REQUEST,
+      };
+      const test = reducer(initialState, action);   
+      expect(test.loading).toEqual(true);
+    });
+    it('should return newState.loading = true', () => {
+      const action = {
+        type: types.ADD_COMMENT_SUCCESS,
+        data: 'comment'
+      };
+      const test = reducer(initialState, action); 
+      expect(test.comments).toEqual(['comment']);
+    });
+    it('returns an error if passed the error action', () => {
+      const action = {
+        type: types.ADD_COMMENT_ERROR,
+        data: 'This is an error message'
+      };
+      const test = reducer(initialState, action);
+      expect(test.error).toEqual('This is an error message');
     });
   });
 });
