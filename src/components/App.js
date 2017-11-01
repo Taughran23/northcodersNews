a6553c89f5a616b3a34f4f0e1fe15a56011636fb
Leo Duffy <leoduffy@hotmail.com>
2017-11-01 14:18:13 +0000

    Add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
-import '../css/bulma.css';
 
-
+import Header from './Header';
 import ArticleList from './ArticleList';
 import TopicsHeader from './TopicsHeader';
 import TopicPage from './TopicPage';
+import UserProfile from './UserProfile';
 import Article from './Article';
+
+import '../css/bulma.css';
+import '../acc/App.css';
 
 const history = createBrowserHistory();
 
@@ -16,9 +19,11 @@ class App extends React.Component {
     return (
       <Router history={history}>
         <div>
+          <Header/>
           <TopicsHeader/>
           <Route exact path='/' component={ArticleList} />
           <Route exact path='/:topic_slug' component={TopicPage} />
+          <Route exact path='/users/:username' component={UserProfile} />
           <Route exact path='/articles/:article_id' component={Article} />
         </div>
       </Router>
