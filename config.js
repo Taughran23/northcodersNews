066984ed9c628704ba2ed13c037cb3e1af463a58
Leo Duffy <leoduffy@hotmail.com>
2017-11-02 16:56:44 +0000

    Remove notes

--- config.js
+++ config.js
@@ -3,8 +3,3 @@ module.exports = {
   USERNAME: 'northcoder'
 };
 
-// looking after different oarts of the site, baskwet team, checkout team
-// autonomy. teams product owners. cross functional teams
-// external companies coming in , spotify. talkin gabout setup.
-
-// .net company on the back end.
