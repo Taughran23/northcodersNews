240482f1b9975576dbd7df48b9ba2d09c41f5186
Leo Duffy <leoduffy@hotmail.com>
2017-11-03 16:05:47 +0000

    Add registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 // import logger from 'redux-logger';
-
+import registerServiceWorker from './registerServiceWorker';
 import reducer from './reducer/reducer';
 import App from './components/App';
 
@@ -12,4 +12,5 @@ const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}>
   <App/>
-</Provider>, document.getElementById('app'));
+</Provider>, document.getElementById('root'));
+registerServiceWorker();
