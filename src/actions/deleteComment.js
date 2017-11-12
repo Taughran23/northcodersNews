24be7c84686ccbb2e02ddf42106915c5ddcbd52e
Leo Duffy <leoduffy@hotmail.com>
2017-11-12 16:06:41 +0000

    Implement deleteComment

---
+++ src/actions/deleteComment.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import * as types from './types';
+
+import {
+  ROOT
+} from '../config';
+
+export function commentDelete (id) {
+  return function (dispatch) {
+    dispatch(commentDeleteRequest());
+    axios.delete(`${ROOT}/comments/${id}`)
+      .then(() => {
+        dispatch(commentDeleteSuccess(id));
+      })
+      .catch((err) => {
+        dispatch(commentDeleteError(err));
+      });
+  };
+}
+
+export function commentDeleteRequest () {
+  return {
+    type: types.COMMENT_DELETE_REQUEST
+  };
+}
+
+export function commentDeleteSuccess (id) {
+  return {
+    type: types.COMMENT_DELETE_SUCCESS,
+    id 
+  };
+}
+
+export function commentDeleteError (error) {
+  return {
+    type: types.COMMENT_DELETE_ERROR,
+    data: error
+  };
+}
