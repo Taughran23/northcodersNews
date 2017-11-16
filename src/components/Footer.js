73d26f6fb74eb443151dd9083104bab4c3b05e1f
Leo Duffy <leoduffy@hotmail.com>
2017-11-16 10:08:45 +0000

    Add github and linkedIn links

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/Footer.css'; 
+
+const Footer = () => (
+  <section>
+    <div className='Footer'>
+     Created by Leo Duffy
+      <a href="https://github.com/Taughran23">
+        <i className="fa fa-github" id='icon' aria-hidden="true" ></i>
+      </a>
+      <a href ="https://www.linkedin.com/in/leo-duffy-a6ba0568" >
+        <i className="fa fa-linkedin-square" id='icon' aria-hidden="true"></i>
+      </a>
+    </div>
+  </section>
+);
+
+export default Footer;
